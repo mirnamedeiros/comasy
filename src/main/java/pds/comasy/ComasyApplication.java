5833e629d61b582a60018c9401428781199ee141
mirna <mirna.medeiros.053@imd.local>
2024-03-21 09:22:04 -0300

    project initialization

---
+++ src/main/java/pds/comasy/ComasyApplication.java
@@ -0,0 +1,13 @@
+package pds.comasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pds/comasy/ComasyApplicationTests.java
@@ -0,0 +1,13 @@
+package pds.comasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
