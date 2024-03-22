b4178ea36c65464e4330735a871382becd55586c
mirnamedeiros <mirnagmedeiros@gmail.com>
2024-03-22 13:53:44 -0300

    (CRUD USUARIO) get and update person implementation

--- src/main/java/pds/comasy/controller/PersonController.java
+++ src/main/java/pds/comasy/controller/PersonController.java
@@ -2,7 +2,10 @@ package pds.comasy.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +26,18 @@ public class PersonController {
     @PostMapping
     public ResponseEntity<PersonDto> addPerson(@RequestBody PersonDto personDto) {
         return new ResponseEntity<>(personService.createPerson(personDto), HttpStatus.CREATED);
+    }
+
+    //GET PERSON
+    @GetMapping("/{cpf}")
+    public ResponseEntity<PersonDto> getPersonByCpf(@PathVariable String cpf) throws Exception {
+        return ResponseEntity.ok(personService.getPersonByCpf(cpf));
+    }
+
+    // UPDATE PERSON
+    @PutMapping("/{cpf}")
+    public ResponseEntity<PersonDto> updatePerson(@PathVariable String cpf, @RequestBody PersonDto personDto) throws Exception {
+        personDto.setCpf(cpf);
+        return ResponseEntity.ok(personService.updatePerson(personDto));
     }
 }
--- src/main/java/pds/comasy/service/PersonService.java
+++ src/main/java/pds/comasy/service/PersonService.java
@@ -1,8 +1,37 @@
 package pds.comasy.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import pds.comasy.dto.PersonDto;
+import pds.comasy.entity.Person;
+import pds.comasy.mapper.PersonMapper;
+import pds.comasy.repository.PersonRepository;
 
-public interface PersonService {
+@Service
+public class PersonService {
 
-    PersonDto createPerson(PersonDto person);
+    @Autowired
+    private PersonRepository personRepository;
+
+    public PersonDto createPerson(PersonDto personDto) {
+        Person person = PersonMapper.mapToPerson(personDto);
+        return PersonMapper.mapToPersonDto(personRepository.save(person));
+    }
+
+    public PersonDto getPersonByCpf(String cpf) throws Exception {
+        Person person = personRepository.findById(cpf).orElseThrow(() ->
+            new Exception("Person doesn't exist")
+        );
+        return PersonMapper.mapToPersonDto(person);
+    }
+
+    public PersonDto updatePerson(PersonDto personDto) throws Exception {
+        PersonDto person;
+        if(personRepository.findById(personDto.getCpf()).isPresent()) {
+            person = personDto;
+        } else {
+            throw new Exception("Person not found");
+        }
+        return PersonMapper.mapToPersonDto(personRepository.save(PersonMapper.mapToPerson(person)));
+    }
 }
