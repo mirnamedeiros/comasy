7eb45fc43a69c98ac7fc6ef324a9f8c8bb016d33
mirnamedeiros <mirnagmedeiros@gmail.com>
2024-05-19 22:04:39 -0300

    #5 Implementada a autenticaçao de usuário com determinação de papeis e restricão de visualização das funcionalidades por papel

--- src/main/java/pds/comasy/service/TokenService.java
+++ src/main/java/pds/comasy/service/TokenService.java
@@ -4,47 +4,44 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import pds.comasy.entity.UserAuthentication;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Collection;
 
-@Service
-public class TokenService {
-    @Value("${api.security.token.secret}")
-    private String secret;
+@Component
+public class AuthProviderService extends AuthProviderService {
 
-    public String generateToken(UserAuthentication user){
-        try{
-            Algorithm algorithm = Algorithm.HMAC256(secret);
-            String token = JWT.create()
-                    .withIssuer("comasy-api")
-                    .withSubject(user.getUsername())
-                    .withExpiresAt(genExpirationDate())
-                    .sign(algorithm);
-            return token;
-        } catch (JWTCreationException exception) {
-            throw new RuntimeException("Error while generating token", exception);
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @Override
+    public Authentication authenticate(Authentication auth) throws AuthenticationException {
+
+        String login = auth.getName();
+        String senha = auth.getCredentials().toString();
+
+        Usuario usuario = usuarioService.findByLogin(login);
+
+        if (usuario != null) {
+            if (verificarSenha(usuario, senha)) {
+                Collection<? extends GrantedAuthority> authorities = usuario.getPermissoes();
+                return new UsernamePasswordAuthenticationToken(usuario, senha, authorities);
+            } else {
+                throw new UsernameNotFoundException("Login e/ou senha inválidos.");
+            }
         }
-    }
-
-    public String validateToken(String token){
-        try {
-            Algorithm algorithm = Algorithm.HMAC256(secret);
-            return JWT.require(algorithm)
-                    .withIssuer("comasy-api")
-                    .build()
-                    .verify(token)
-                    .getSubject();
-        } catch (JWTVerificationException exception){
-            return "";
-        }
-    }
-
-    private Instant genExpirationDate(){
-        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+        throw new UsernameNotFoundException("Login e/ou senha inválidos.");
     }
 }
